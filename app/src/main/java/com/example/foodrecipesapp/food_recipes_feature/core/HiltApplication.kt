247f83792cefe9b44d36b6a82ff87b460f322f0e
Nadine Islam <nadeen.kamaal@gmail.com>
2023-10-31 15:55:02 +0200

    Initial commit

--- app/src/main/java/com/example/foodrecipesapp/food_recipes_feature/core/HiltApplication.kt
+++ app/src/main/java/com/example/foodrecipesapp/food_recipes_feature/core/HiltApplication.kt
@@ -1,4 +1,4 @@
-package com.example.foodrecipesapp.food_recipes_feature.core
+package com.example.foodrecipesapp.food_recipes_feature.core//package com.example.foodrecipesapp.core
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
