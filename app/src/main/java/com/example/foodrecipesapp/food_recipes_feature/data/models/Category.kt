6fd8852960dbd0915a97f5dfab28cf83f104f107
Nadine Islam <nadeen.kamaal@gmail.com>
2023-11-28 17:18:40 +0200

    removing some unnecessary attributes

--- app/src/main/java/com/example/foodrecipesapp/food_recipes_feature/data/models/Category.kt
+++ app/src/main/java/com/example/foodrecipesapp/food_recipes_feature/data/models/Category.kt
@@ -3,6 +3,5 @@ package com.example.foodrecipesapp.food_recipes_feature.data.models
 data class Category(
     val idCategory: String?,
     val strCategory: String?,
-    val strCategoryDescription: String?,
     val strCategoryThumb: String?
 )
