58d1a31a58d5836028e936d5b26667c27cf164c3
Nadine Islam <nadeen.kamaal@gmail.com>
2024-01-05 17:48:08 +0200

    Removed dexOutputDir initialization and setReadOnly() method call

--- app/src/main/java/com/example/foodrecipesapp/food_recipes_feature/presentation/activities/MainActivity.kt
+++ app/src/main/java/com/example/foodrecipesapp/food_recipes_feature/presentation/activities/MainActivity.kt
@@ -8,19 +8,16 @@ import com.example.foodrecipesapp.food_recipes_feature.presentation.components.M
 import com.example.foodrecipesapp.food_recipes_feature.presentation.viewmodels.HomeViewModel
 import com.example.foodrecipesapp.ui.theme.FoodRecipesAppTheme
 import dagger.hilt.android.AndroidEntryPoint
-import java.io.File
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-    private val viewModel :HomeViewModel by viewModels()
+    private val viewModel: HomeViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val dexOutputDir: File = codeCacheDir
-        dexOutputDir.setReadOnly()
         setContent {
             FoodRecipesAppTheme {
                 MainScreen(viewModel)
-                }
+            }
 
         }
     }
